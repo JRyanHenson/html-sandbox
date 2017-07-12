66b49f752060306ac79a2adfaf368c81e3b67cde
Rochelle Lewis <rlewis37@cnm.edu>
2017-07-12 13:59:56 -0600

    added table, ul and ol

--- index.php
+++ index.php
@@ -5,6 +5,43 @@
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>This is my web page</h1>
+		<h1>This is my web page.</h1>
+
+		<table>
+			<tr>
+				<th>Cats</th>
+				<th>Dogs</th>
+			</tr>
+			<tr>
+				<td>Persian</td>
+				<td>Beagle</td>
+			</tr>
+			<tr>
+				<td>Maine Coon</td>
+				<td>Rottweiler</td>
+			</tr>
+			<tr>
+				<td>Siamese</td>
+				<td>German Shepard</td>
+			</tr>
+		</table>
+
+		<!-- This is an HTML comment -->
+		<h2>A Few of My Favorite Things</h2>
+		<ul>
+			<li>Tacos
+			<li>The Beach</li>
+			<li>Kittens</li>
+			<li>Fast Cars</li>
+		</ul>
+
+		<h2>Things to Do Today</h2>
+		<ol>
+			<li>Feed the kitty.</li>
+			<li>Give Dylan a hard time.</li>
+			<li>Water the plants.</li>
+			<li>Hack vulnerabilites in bootcamp-coders.</li>
+		</ol>
+
 	</body>
 </html>
