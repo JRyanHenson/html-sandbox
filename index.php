d773fbee3f6ef77f577c2e9e3cc6e1bed623d727
Rochelle Lewis <rlewis37@cnm.edu>
2017-07-31 11:45:08 -0600

    added bootstrap form

--- index.php
+++ index.php
@@ -94,8 +94,43 @@
 						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
 					</div>
 				</div><!--/.row-->
+
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Contact Me!</h2>
+						<!-- BEGIN FORM -->
+						<form>
+							<div class="form-group">
+								<label for="txtName">Your Name</label>
+								<input id="txtName" name="txtName" class="form-control" type="text" placeholder="Name">
+							</div>
+							<div class="form-group">
+								<label for="emailEmail">Email Address</label>
+								<input id="emailEmail" name="emailEmail" class="form-control" type="email" placeholder="Email Address">
+							</div>
+							<div class="form-group">
+								<label for="txtSubject">Subject</label>
+								<input id="txtSubject" name="txtSubject" class="form-control" type="text" placeholder="Subject">
+							</div>
+							<div class="form-group">
+								<label for="txtMessage">Message</label>
+								<textarea id="txtMessage" name="txtMessage" class="form-control" cols="30" rows="10"></textarea>
+							</div>
+							<button class="btn btn-info" type="submit">Submit</button>
+							<button class="btn btn-default" type="reset">Reset</button>
+						</form>
+					</div>
+					<div class="col-md-7 col-md-offset-1">
+						<h2>More Info</h2>
+						<p>Sensors indicate human life forms 30 meters below the planet's surface. Stellar flares are increasing in magnitude and frequency. Set course for Rhomboid Dronegar 006, warp seven. There's no evidence of an advanced communication network. Total guidance system failure, with less than 24 hours' reserve power. Shield effectiveness has been reduced 12 percent. We have covered the area in a spherical pattern which a ship without warp drive could cross in the given time.</p>
+						<p>Sensors indicate no shuttle or other ships in this sector. According to coordinates, we have travelled 7,000 light years and are located near the system J-25. Tractor beam released, sir. Force field maintaining our hull integrity. Damage report? Sections 27, 28 and 29 on decks four, five and six destroyed. Without our shields, at this range it is probable a photon detonation could destroy the Enterprise.</p>
+						<p>Unidentified vessel travelling at sub warp speed, bearing 235.7. Fluctuations in energy readings from it, Captain. All transporters off. A strange set-up, but I'd say the graviton generator is depolarized. The dark colourings of the scrapes are the leavings of natural rubber, a type of non-conductive sole used by researchers experimenting with electricity. The molecules must have been partly de-phased by the anyon beam.</p>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
+				</div>
+
 			</div><!-- /.container -->
-		</div>
+		</div><!--/.sfooter-content-->
 
 		<footer>
 			<div class="container">
