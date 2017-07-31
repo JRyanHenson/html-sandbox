22438e263afdb54325c77f3f9500b990a6498195
Rochelle Lewis <rlewis37@cnm.edu>
2017-07-31 11:15:31 -0600

    added sticky footer

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
 		<!-- CUSTOM CSS HERE -->
+		<link rel="stylesheet" href="css/style.css">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!--[if lt IE 9]>
@@ -30,53 +31,71 @@
 
 		<title>Bootstrap Practice</title>
 	</head>
-	<body>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<header>
+				<nav class="navbar navbar-default">
+					<div class="container">
+						<!-- Brand and toggle get grouped for better mobile display -->
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-brand" href="#">Bootstrap Sandbox</a>
+						</div>
 
-		<header>
+						<!-- Collect the nav links, forms, and other content for toggling -->
+						<div class="collapse navbar-collapse" id="main-nav">
+							<ul class="nav navbar-nav navbar-right">
+								<li><a href="https://www.abebooks.com/" target="_blank">AbeBooks</a></li>
+								<li><a href="https://www.groupon.com/" target="_blank">Groupon</a></li>
+								<li><a href="https://www.jetpens.com/" target="_blank">JetPens</a></li>
+								<li><a href="https://customcultsnowboards.com/index.php" target="_blank">CustomCult</a></li>
+							</ul>
+						</div><!-- /.navbar-collapse -->
+					</div><!-- /.container-fluid -->
+				</nav>
+			</header>
+
+			<!-- begin grid layout -->
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12">
-						<h1>Hello Bootstrap!</h1>
+					<div class="col-md-6">
+						<h2>Column 1</h2>
+						<p>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
 					</div>
-				</div>
-			</div>
-		</header>
+					<div class="col-md-6">
+						<h2>Column 2</h2>
+						<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal.</p>
+					</div><!-- /.col-md-6 -->
+				</div><!-- /.row -->
 
-		<!-- begin grid layout -->
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<h2>Column 1</h2>
-					<p>Deflector power at maximum. Energy discharge in six seconds. Warp reactor core primary coolant failure. Fluctuate phaser resonance frequencies. Resistance is futile. Recommend we adjust shield harmonics to the upper EM band when proceeding. These appear to be some kind of power-wave-guide conduits which allow them to work collectively as they perform ship functions. Increase deflector modulation to upper frequency band.</p>
-				</div>
-				<div class="col-md-6">
-					<h2>Column 2</h2>
-					<p>We're acquainted with the wormhole phenomenon, but this... Is a remarkable piece of bio-electronic engineering by which I see much of the EM spectrum ranging from heat and infrared through radio waves, et cetera, and forgive me if I've said and listened to this a thousand times. This planet's interior heat provides an abundance of geothermal energy. We need to neutralize the homing signal.</p>
-				</div><!-- /.col-md-6 -->
-			</div><!-- /.row -->
+				<div class="row">
+					<div class="col-md-12 hidden-xs">
+						<h2>Column 3</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off. Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div><!--/.row-->
+				</div><!-- /.row -->
 
-			<div class="row">
-				<div class="col-md-12 hidden-xs">
-					<h2>Column 3</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off. Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+				<div class="row">
+					<div class="col-md-4">
+						<h2>Column 4</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
+					<div class="col-md-4">
+						<h2>Column 5</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
+					<div class="col-md-4">
+						<h2>Column 6</h2>
+						<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
+					</div>
 				</div><!--/.row-->
-			</div><!-- /.row -->
-
-			<div class="row">
-				<div class="col-md-4">
-					<h2>Column 4</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-				</div>
-				<div class="col-md-4">
-					<h2>Column 5</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-				</div>
-				<div class="col-md-4">
-					<h2>Column 6</h2>
-					<p>Shields up. I recommend we transfer power to phasers and arm the photon torpedoes. Something strange on the detector circuit. The weapons must have disrupted our communicators. You saw something as tasty as meat, but inorganically materialized out of patterns used by our transporters. Captain, the most elementary and valuable statement in science, the beginning of wisdom, is 'I do not know.' All transporters off.</p>
-				</div>
-			</div><!--/.row-->
-		</div><!-- /.container -->
+			</div><!-- /.container -->
+		</div>
 
 		<footer>
 			<div class="container">
